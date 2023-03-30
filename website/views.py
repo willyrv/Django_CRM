cca504ad28771b60286e7f0cc73cc7f2ee241cb3
Willy RODRIGUEZ <willyrv@gmail.com>
2023-03-31 01:20:27 +0200

    Working on the Register feature

--- website/views.py
+++ website/views.py
@@ -28,3 +28,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out ...")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
