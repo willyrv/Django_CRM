c7eab74fbbed47e77920e1569981e48992997f76
Willy RODRIGUEZ <willyrv@gmail.com>
2023-04-03 01:01:35 +0200

    Added the view containing the table with the client records

--- website/views.py
+++ website/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if loggin in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +23,7 @@ def home(request):
             messages.error(request, "Loggin error, please try again ...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def login_user(request):
     pass
